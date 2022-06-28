a6dd4d4e91d16fc0293c4013aa49d64a2bec8b10
Muji <rasmusiam@gmail.com>
2022-06-28 03:19:03 +0100

    skybox added

---
+++ CranialLump-SusSkelSubmission/Assets/Scripts/Health Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : HealthMONO
+{
+    // Start is called before the first frame update
+    void Start1()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update1()
+    {
+        base.Update();
+    }
+}
