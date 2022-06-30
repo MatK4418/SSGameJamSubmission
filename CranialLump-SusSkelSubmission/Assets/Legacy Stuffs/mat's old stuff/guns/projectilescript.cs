472747363a532204800a6f40d57a50d3c5bb95d2
MatK4418 <potatomagnet68@gmail.com>
2022-06-30 16:20:27 +0100

    Added Particles to Bullets + Fixed Shotgun Pellet Rotation

--- CranialLump-SusSkelSubmission/Assets/Legacy Stuffs/mat's old stuff/guns/projectilescript.cs
+++ CranialLump-SusSkelSubmission/Assets/Legacy Stuffs/mat's old stuff/guns/projectilescript.cs
@@ -32,29 +32,17 @@ public class projectilescript : MonoBehaviour
     void OnCollisionEnter(Collision other)
     {
         ContactPoint contact = other.GetContact(0);
-
         
-        if (other.gameObject.tag == "Gun")
+        if (other.gameObject.tag == "Enemy")
         {
-            Physics.IgnoreCollision(other.transform.GetComponent<Collider>(), bCollider);
-        }
-        
-        else if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Light Enemy" || other.gameObject.tag == "Heavy Enemy")
-        {
-
-            
+            Instantiate(enemyParticles, contact.point, Quaternion.LookRotation(contact.normal, Vector3.up)); // Spawns blood splatter. 
             Destroy(gameObject);
-            //switched off for the moment 
-            //Instantiate(enemyParticles, contact.point, Quaternion.LookRotation(contact.normal, Vector3.up)); // Spawns blood splatter. 
-            
         }
 
         else if (other.gameObject.tag == "Environment")
         {
-            //Instantiate(enviroParticles, contact.point, Quaternion.LookRotation(contact.normal, Vector3.up)); 
-            //switching off for the time being because don't have particles in game
+            Instantiate(enviroParticles, contact.point, Quaternion.LookRotation(contact.normal, Vector3.up)); 
             Destroy(gameObject);
-
             //Credit for learning about contact points: https://www.youtube.com/watch?v=2bPd_dmqGuM
         }
     }
